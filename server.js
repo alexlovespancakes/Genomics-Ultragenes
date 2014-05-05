8d630b3c22065900b0509567fa53827bae940acd
otternq <otternq@gmail.com>
2014-05-04 17:14:09 -0700

    updating for heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/src'));
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
+
+console.log("Server running at http://127.0.0.1:"+port+"/");
+console.log("To stop the server: CTRL+c");
